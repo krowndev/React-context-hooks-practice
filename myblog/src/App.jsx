d09ed0fc4782c88b299d230b162d6e264503c690
krowndev <tariqtahir43@yahoo.com>
2022-11-30 19:25:07 +0500

    start project

---
+++ myblog/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>all blogs</h1>
+          <div className="container bg-warning  ">
+            testing bootstrap
+          </div>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
